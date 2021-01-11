2e2cd6b341675388d4d55f15e11839f4bdc5d99a
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-01-11 16:21:06 +0000

    Add kernel request

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,30 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, InputDialog } from '@jupyterlab/apputils';
+
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { IMainMenu } from '@jupyterlab/mainmenu';
+
+import { ITranslator } from '@jupyterlab/translation';
+
+import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
+
+import { Menu } from '@lumino/widgets';
+
+import { ExamplePanel } from './panel';
+
 import { requestAPI } from './handler';
+
+/**
+ * The command IDs used by the console plugin.
+ */
+namespace CommandIDs {
+  export const create = 'kernel-output:create';
+
+  export const execute = 'kernel-output:execute';
+}
 
 /**
  * Initialization data for the node_editor extension.
@@ -11,19 +34,106 @@ import { requestAPI } from './handler';
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'node_editor:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension node_editor is activated!');
-
-    requestAPI<any>('get_example')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(reason => {
-        console.error(
-          `The node_editor server extension appears to be missing.\n${reason}`
-        );
-      });
-  }
+  optional: [ILauncher],
+  requires: [ICommandPalette, IMainMenu, IRenderMimeRegistry, ITranslator],
+  activate: activate
 };
+
+/**
+ * Activate the JupyterLab extension.
+ *
+ * @param app Jupyter Front End
+ * @param palette Jupyter Commands Palette
+ * @param mainMenu Jupyter Menu
+ * @param rendermime Jupyter Render Mime Registry
+ * @param translator Jupyter Translator
+ * @param launcher [optional] Jupyter Launcher
+ */
+function activate(
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  mainMenu: IMainMenu,
+  rendermime: IRenderMimeRegistry,
+  translator: ITranslator,
+  launcher: ILauncher | null
+): void {
+  console.log('JupyterLab extension node_editor is activated!');
+
+  const manager = app.serviceManager;
+  const { commands, shell } = app;
+  const category = 'Extension Examples';
+  const trans = translator.load('jupyterlab');
+  let panel: ExamplePanel;
+
+  /**
+   * Creates a example panel.
+   *
+   * @returns The panel
+   */
+  async function createPanel(): Promise<ExamplePanel> {
+    panel = new ExamplePanel(manager, rendermime, translator);
+    shell.add(panel, 'main');
+    return panel;
+  }
+
+  // add menu tab
+  const exampleMenu = new Menu({ commands });
+  exampleMenu.title.label = trans.__('Kernel Output');
+  mainMenu.addMenu(exampleMenu);
+
+  // add commands to registry
+  commands.addCommand(CommandIDs.create, {
+    label: trans.__('Open the Kernel Output Panel'),
+    caption: trans.__('Open the Kernel Output Panel'),
+    execute: createPanel
+  });
+
+  commands.addCommand(CommandIDs.execute, {
+    label: trans.__('Contact Kernel and Execute Code'),
+    caption: trans.__('Contact Kernel and Execute Code'),
+    execute: async () => {
+      // Create the panel if it does not exist
+      if (!panel) {
+        await createPanel();
+      }
+      // Prompt the user about the statement to be executed
+      const input = await InputDialog.getText({
+        title: trans.__('Code to execute'),
+        okLabel: trans.__('Execute'),
+        placeholder: trans.__('Statement to execute')
+      });
+      // Execute the statement
+      if (input.button.accept) {
+        const code = input.value;
+        panel.execute(code);
+      }
+    }
+  });
+
+  // add items in command palette and menu
+  [CommandIDs.create, CommandIDs.execute].forEach(command => {
+    palette.addItem({ command, category });
+    exampleMenu.addItem({ command });
+  });
+
+  // Add launcher
+  if (launcher) {
+    launcher.add({
+      command: CommandIDs.create,
+      category: category
+    });
+  }
+
+  // Request something simple on the API
+  requestAPI<any>('get_example')
+    .then(data => {
+      console.log(data);
+    })
+    .catch(reason => {
+      console.error(
+        `The node_editor server extension appears to be missing.\n${reason}`
+      );
+    });
+}
 
 export default extension;
---
+++ src/panel.ts
@@ -0,0 +1,100 @@
+import {
+  ISessionContext,
+  SessionContext,
+  sessionContextDialogs
+} from '@jupyterlab/apputils';
+
+import { OutputAreaModel, SimplifiedOutputArea } from '@jupyterlab/outputarea';
+
+import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
+
+import { KernelMessage, ServiceManager } from '@jupyterlab/services';
+import {
+  ITranslator,
+  nullTranslator,
+  TranslationBundle
+} from '@jupyterlab/translation';
+
+import { Message } from '@lumino/messaging';
+
+import { StackedPanel } from '@lumino/widgets';
+
+/**
+ * The class name added to the example panel.
+ */
+const PANEL_CLASS = 'jp-RovaPanel';
+
+/**
+ * A panel with the ability to add other children.
+ */
+export class ExamplePanel extends StackedPanel {
+  constructor(
+    manager: ServiceManager.IManager,
+    rendermime: IRenderMimeRegistry,
+    translator?: ITranslator
+  ) {
+    super();
+    this._translator = translator || nullTranslator;
+    this._trans = this._translator.load('jupyterlab');
+    this.addClass(PANEL_CLASS);
+    this.id = 'kernel-output-panel';
+    this.title.label = this._trans.__('Kernel Output Example View');
+    this.title.closable = true;
+
+    this._sessionContext = new SessionContext({
+      sessionManager: manager.sessions,
+      specsManager: manager.kernelspecs,
+      name: 'Kernel Output'
+    });
+
+    this._outputareamodel = new OutputAreaModel();
+    this._outputarea = new SimplifiedOutputArea({
+      model: this._outputareamodel,
+      rendermime: rendermime
+    });
+
+    this.addWidget(this._outputarea);
+
+    void this._sessionContext
+      .initialize()
+      .then(async value => {
+        if (value) {
+          await sessionContextDialogs.selectKernel(this._sessionContext);
+        }
+      })
+      .catch(reason => {
+        console.error(
+          `Failed to initialize the session in ExamplePanel.\n${reason}`
+        );
+      });
+  }
+
+  get session(): ISessionContext {
+    return this._sessionContext;
+  }
+
+  dispose(): void {
+    this._sessionContext.dispose();
+    super.dispose();
+  }
+
+  execute(code: string): void {
+    SimplifiedOutputArea.execute(code, this._outputarea, this._sessionContext)
+      .then((msg: KernelMessage.IExecuteReplyMsg) => {
+        console.log(msg);
+      })
+      .catch(reason => console.error(reason));
+  }
+
+  protected onCloseRequest(msg: Message): void {
+    super.onCloseRequest(msg);
+    this.dispose();
+  }
+
+  private _sessionContext: SessionContext;
+  private _outputarea: SimplifiedOutputArea;
+  private _outputareamodel: OutputAreaModel;
+
+  private _translator: ITranslator;
+  private _trans: TranslationBundle;
+}
